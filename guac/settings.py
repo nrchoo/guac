e4bb3eb4877696332f6f99a84df2176eb523305b
nathan <nrchoo@gmail.com>
2017-06-15 00:09:44 -0700

    using new db for cloud

--- guac/settings.py
+++ guac/settings.py
@@ -74,15 +74,15 @@ WSGI_APPLICATION = 'guac.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
 
+'''
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
@@ -93,7 +93,7 @@ DATABASES = {
         "PORT": "5432",
     }
 }
-
+'''
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
